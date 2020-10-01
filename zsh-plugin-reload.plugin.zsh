7f695925f5b48ec2fbad21eb4e021e50d0fcaa00
Aubrey Portwood <code@aubreypwd.com>
2020-09-30 20:50:32 -0600

    Fix docblock.

--- zsh-plugin-reload.plugin.zsh
+++ zsh-plugin-reload.plugin.zsh
@@ -5,11 +5,15 @@
  #
  # E.g: reload
  #
+ # @author Aubrey Portwood <code@aubreypwd.com>
  # @since 06-09-2019
+ # @since 1.0.0
  ##
 function reload {
 	if ! [[ -e "$HOME/.zshrc.secure" ]]; then
-		source "$HOME/.zshrc.secure" # Create a ~/.zshrc.secure file and we will also source that.
+
+		# Create a ~/.zshrc.secure file and we will also source that.
+		source "$HOME/.zshrc.secure"
 	fi
 
 	source "$HOME/.zshrc"
