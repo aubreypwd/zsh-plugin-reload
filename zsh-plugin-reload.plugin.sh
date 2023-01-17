42a4b7c7e318ab9ec1c1cb853ff6e91ad17ba08e
Aubrey Portwood <code@aubreypwd.com>
2023-01-16 20:23:11 -0700

    Add support for omz reload.

---
+++ zsh-plugin-reload.plugin.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+###
+ # Reload Config
+ #
+ # E.g: reload
+ #
+ # @author Aubrey Portwood <code@aubreypwd.com>
+ # @since 06-09-2019
+ # @since 1.0.0
+ # @since 1.1.0 Added support for omz reload.
+ ##
+reload () {
+
+	if test ! -x "$(command -v "omz")"; then
+
+		omz reload && \
+			return 0
+	fi
+
+	. "$HOME/.zshrc"
+
+	echo "Sourced $HOME/.zshrc"
+}
